a934162c6f386e2be49c2ce616f035b4d237bf3e
XTKJ1 <nansekolah@gmail.com>
2024-10-30 20:24:18 +0700

    Delete agenda/database.js

--- agenda/database.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    multipleStatements  : true,
-    host                : 'remotemysql.com',
-    user                : 'byMWsNlDww',
-    password            : 'PLJiPdPZ32',
-    database            : 'byMWsNlDww'
-};
